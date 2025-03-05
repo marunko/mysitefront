0a146135c5d23d3b18f07e076660163bf48a43e5
Paul Marunko <marunkopavlo@gmail.com>
2025-03-05 00:32:22 -0700

    ff1

--- pages/api/save-key.ts
+++ pages/api/save-key.ts
@@ -18,7 +18,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-      sameSite: 'lax',
+      sameSite: 'none',
     });
     console.log("SETTED COOKIES " + key);
     console.log(process.env.NODE_ENV);
